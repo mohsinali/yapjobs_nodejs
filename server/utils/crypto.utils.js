8da9c0eb6e7030d807dac2102899b9e5dc9585ff
isaadabbasi <itssaadabbasi@gmail.com>
2017-10-05 12:05:20 +0500

    refactored+consized cryptoHandler module

--- server/utils/crypto.utils.js
+++ server/utils/crypto.utils.js
@@ -8,34 +8,17 @@ class CryptoUtils {
         console.log('JOINT TO CRYPTO UTILS MADE...');
     }    
 
-    assignStandardJWT(payload = {}, enc_type = process.env.ENC_TYPE){
-        return new Promise((resolve, reject) => {
-            signJWT(payload, process.env.SECRET_KEY , {
-                algorithm: enc_type,
-                expiresIn: 60*24
-            }).then(token => {
-                resolve(token)
-            }).catch(err=>{
-                reject(err)
-            })    
-        });
-    }
-    
-    verifyJWT(token){
-        try{
-            let 
-                decodedToken = jwt.verify(token, process.env.SECRET_KEY),
-                isTokenValid = '_id' in decodedToken;
-            return {
-                isTokenValid,
-                body: decodedToken
-            };
-        } catch(e){
-            return {
-                isTokenValid: false,
-                body: "INVALID SIGNATURE"
-            }
+    assignStandardJWT(
+        payload = {},
+        algorithm = process.env.ENC_TYPE,
+        ){
+        const claims = {
+            algorithm,
+            expiresIn: 60*60,
+            subject: payload.email || null,
+            jwtid: payload.id || null
         }
+        return signJWT(payload, process.env.SECRET_KEY , claims);
     }
     
 }
