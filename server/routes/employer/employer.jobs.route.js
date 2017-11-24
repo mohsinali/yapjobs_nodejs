e26b15059db22140f4e8944be76a855f770848ca
isaadabbasi <itssaadabbasi@gmail.com>
2017-11-24 21:33:44 +0500

    extended employer route, and added addjob route

--- server/routes/employer/employer.jobs.route.js
+++ server/routes/employer/employer.jobs.route.js
@@ -1,3 +1,28 @@
 const 
     express = require('express'),
+    router = express.Router(),
+    JobJoint = require('../../joints/jobs.joints');
     
+    addJob = async (request, response) => {
+        let {
+            title = null, category = null, additional_info = '',
+            contract = null, salary = null, manager_info = null,
+            total_applicants = [], location = {}, invitation_link = null
+        }
+
+        let jobModel = {
+            title, category, additional_info, contract, 
+            salary, manager_info, total_applicants, location, invitation_link
+        }
+
+        try {
+            const {status, body} = await JobJoint.save(jobModel);
+            res.status(status).send(body);
+
+        } catch( e ){
+            console.error(e);
+        }
+    };
+
+router.post('/add', addJob);
+module.exports = router;
--- server/routes/employer/employers.route.js
+++ server/routes/employer/employers.route.js
@@ -1,10 +1,11 @@
 const
     express = require('express'),
     router = express.Router(),
-    EmployersJoint = require('../joints/employers.joint'),
-    // EmployerJobs = require(),
-    { commonCallback, checkTokenInRequest } = require('../utils/common.utils');
+    EmployersJoint = require('../../joints/employers.joint'),
+    EmployerJobs = require('./employer.jobs.route'),
+    { commonCallback, checkTokenInRequest } = require('../../utils/common.utils');
 
 
 
-// router.use('/jobs', )
+router.use('/jobs',EmployerJobs)
+module.exports = router;
